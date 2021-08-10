db3e5ad3d6f7d746f4aa065207d3f4de6ab7386c
Li Yefeng <16411015+l1yefeng@users.noreply.github.com>
2021-08-10 09:59:01 -0400

    Update comments

--- E/dynamics.mli
+++ E/dynamics.mli
@@ -7,12 +7,11 @@ open Statics
 val valu : exp -> bool
 
 (* One step of the transition. PFPL notation:
-    e |---> e'
+    e ⟼ e'
    It's a by-value interpretation.
    Maps to None when stuck or final (val).
  *)
 val transition : exp -> exp option
 
-(* State where the system cannot transit from
-*)
+(* State where the system cannot transit from *)
 val normalize : exp -> exp
